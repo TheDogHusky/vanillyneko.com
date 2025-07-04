3669000e812f275d45bea9adb937ddaa8852c2b6
TheDogHusky <contact@classydev.fr>
2025-07-04 22:33:48 +0200

    initial commit

---
+++ app/error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+const props = defineProps({
+    error: Object as () => NuxtError
+});
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <h1>Error {{ props.error?.statusCode }}</h1>
+            <p>{{ props.error?.message }}</p>
+            <button @click="handleError">Go to Home</button>
+        </div>
+    </main>
+</template>
