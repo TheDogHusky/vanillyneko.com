821c6af0c5fe6062efa6ef41bf6346777d7bfa2d
TheDogHusky <contact@classydev.fr>
2025-07-09 15:02:08 +0200

    feat: add contact page

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -3,5 +3,10 @@
 </script>
 
 <template>
+    <main class="container bg-dark min-h-10xl">
+        <h1 class="font-6xl">Contact Information</h1>
+    </main>
+    <section class="bg-image bg-secondary-wallpaper min-h-screen container">
 
+    </section>
 </template>
