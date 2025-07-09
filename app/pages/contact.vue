0a57e1e6d458987e43c3240145d7500dc6e71228
TheDogHusky <contact@classydev.fr>
2025-07-09 22:37:49 +0200

    feat: finish contact page

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -6,7 +6,45 @@
     <main class="container bg-dark min-h-10xl">
         <h1 class="font-6xl">Contact Information</h1>
     </main>
-    <section class="bg-image bg-secondary-wallpaper min-h-screen container">
-
+    <section class="bg-image bg-secondary-wallpaper min-h-screen container font-2xl gap-2xl">
+        <h2 class="font-5xl">Get in Touch with Us</h2>
+        <div class="contact-list">
+            <div class="contact-info">
+                <Icon name="material-symbols:mail-rounded" />
+                <h2 class="font-4xl">E-Mail</h2>
+                <ul>
+                    <li>
+                        Vanilly's E-Mail: <a href="mailto:nilly@vanillyneko.com">nilly@vanillyneko.com</a>
+                    </li>
+                    <li>
+                        Admins E-Mail: <a href="mailto:admin@vanillyneko.com">admin@vanillyneko.com</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="contact-info">
+                <Icon name="material-symbols:call-sharp" />
+                <h2 class="font-4xl">Phone or Fax</h2>
+                <ul>
+                    <li>
+                        Phone: <a href="tel:+13048079093">+1 (304) 807-9093</a>
+                    </li>
+                    <li>
+                        Fax: <a href="tel:+13044100334">1 (304) 410-0334</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="contact-info">
+                <Icon name="fa6-brands:discord" />
+                <h2 class="font-4xl">Discord</h2>
+                <ul>
+                    <li>
+                        Owner: <code>@vanillyneko</code>
+                    </li>
+                    <li>
+                        Helpers: <code>@leighmagee</code>, <code>@theaxolotlsystem</code>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </section>
 </template>
