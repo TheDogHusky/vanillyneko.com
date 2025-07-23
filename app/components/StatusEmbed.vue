00b02c0b4dfb1f28bf86b1d09682122879c5b677
TheDogHusky <contact@classydev.fr>
2025-07-23 20:35:54 +0200

    fix: iframes

--- app/components/StatusEmbed.vue
+++ app/components/StatusEmbed.vue
@@ -1,26 +1,40 @@
 <script setup lang="ts">
-useHead({
-    script: [
-        {
-            src: "https://stttc.b-cdn.net/iframeResizer/iframeResizer.min.js"
-        }
-    ]
-});
+import "@iframe-resizer/child";
+import IframeResizer from '@iframe-resizer/vue/sfc';
 </script>
 
 <template>
     <div class="status-embeds">
         <div class="status-embed">
-            <iframe class="htframe" src="https://wl.hetrixtools.com/r/a98f2722f4c6da5d0ec3d4f11bfcad06/" width="100%" scrolling="no" style="border:none;" sandbox="allow-scripts allow-same-origin allow-popups" onload="iFrameResize([{log:false}],'.htframe')"></iframe>
+            <IframeResizer
+                src="https://wl.hetrixtools.com/r/a98f2722f4c6da5d0ec3d4f11bfcad06/"
+                width="100%"
+                license="GPLv3"
+                sandbox="allow-scripts allow-same-origin allow-popups"
+            ></IframeResizer>
         </div>
         <div class="status-embed">
-            <iframe class="htframe" src="https://wl.hetrixtools.com/r/695e4b29f1304f3cedb081ef0cb686ba/" width="100%" scrolling="no" style="border:none;" sandbox="allow-scripts allow-same-origin allow-popups" onload="iFrameResize([{log:false}],'.htframe')"></iframe>
+            <IframeResizer
+                src="https://wl.hetrixtools.com/r/695e4b29f1304f3cedb081ef0cb686ba/"
+                width="100%"
+                license="GPLv3"
+                sandbox="allow-scripts allow-same-origin allow-popups"
+            ></IframeResizer>
         </div>
         <div class="status-embed">
-            <iframe class="htframe" src="https://wl.hetrixtools.com/r/a5f8401602b66cca155352d862dc7826/" width="100%" scrolling="no" style="border:none;" sandbox="allow-scripts allow-same-origin allow-popups" onload="iFrameResize([{log:false}],'.htframe')"></iframe>
+            <IframeResizer
+                src="https://wl.hetrixtools.com/r/a5f8401602b66cca155352d862dc7826/"
+                width="100%"
+                license="GPLv3"
+                sandbox="allow-scripts allow-same-origin allow-popups"
+            ></IframeResizer>
         </div>
-        <div class="status-embed">
-            <iframe src="https://up.vnil.me/status/all/" width="100%" height="3000" style="border: none;"></iframe>
+        <div class="status-embed uptime-kuma-embed">
+            <IframeResizer
+                src="https://up.vnil.me/status/all/"
+                width="100%"
+                license="GPLv3"
+            ></IframeResizer>
         </div>
     </div>
 </template>
