ed407d43d79c1a722661e45f4284f101c8f4eadc
vanillyneko <vanillyneko@icloud.com>
2025-11-13 12:41:03 +0000

    Add neko-roots.vue page with admin access links and integrate @nuxtjs/turnstile package.

---
+++ app/pages/neko-roots.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class="center index">
+        <MainWallpaper />
+        <h1 class="font-6xl">Admin Logins!~</h1>
+        <ScrollButton to="#content" />
+    </main>
+    <section class="container min-h-screen bg-image bg-tertiary-wallpaper">
+    <h2 class="font-6xl">Access Links!~</h2>
+    <ul class="font-2xl w-3/4">
+        <li>
+            <p>
+                <span class="font-bold">Radarr</span>: <a href="https://riko.vnil.me" target="_blank">https://riko.vnil.me</a>
+            </p>
+        </li>
+        <br>
+        <li>
+            <p>
+                <span class="font-bold">Sonarr</span>: <a href="https://aiko.vnil.me" target="_blank">https://aiko.vnil.me</a>
+            </p>
+        </li>
+        <br>
+        <li>
+            <p>
+                <span class="font-bold">Prowlarr</span>: <a href="https://chiyo.vnil.me" target="_blank">https://chiyo.vnil.me</a>
+            </p>
+        </li>
+        <br>
+        <li>
+            <p>
+                <span class="font-bold">Downloader</span>: <a href="https://kiyomi.vnil.me" target="_blank">https://kiyomi.vnil.me</a>
+            </p>
+        </li>
+        <br>
+        <li>
+            <p>
+                <span class="font-bold">Requests</span>: <a href="https://yuki.vnil.me" target="_blank">https://yuki.vnil.me</a>
+            </p>
+        </li>
+        <br>
+        <li>
+            <p>
+                <span class="font-bold">AList File Manager</span>: <a href="https://fumiko.vnil.me" target="_blank">https://fumiko.vnil.me</a>
+            </p>
+        </li>
+    </ul>
+    </section>
+</template>
